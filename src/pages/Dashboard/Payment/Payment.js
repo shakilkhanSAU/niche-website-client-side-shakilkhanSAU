2a3e259eada8ba4d66d521313b0e7316619456c5
shakilkhanSAU <shakilkhan.sau@gmail.com>
2022-02-20 00:54:56 +0600

    payment api backend

--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Payment = () => {
     const { orderId } = useParams();
+    const [order, setOrder] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/payment/${orderId}`)
+            .then(res => res.json())
+            .then(data => setOrder(data))
+    }, [orderId])
     return (
         <div>
-            <h2 className="fw-bold text-center pt-3 pb-2">Payment For: {orderId}</h2>
+            <h2 className="fw-bold text-center pt-3 pb-2">Payment For: {order?.name}</h2>
+            <h5 className="fw-bold text-center text-danger pt-3 pb-2">Please pay: ${order?.price}</h5>
+
         </div>
     );
 };
