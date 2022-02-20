2122239a5670b582077e7f852d71f8bb5bcb6f41
shakilkhanSAU <shakilkhan.sau@gmail.com>
2022-02-20 10:41:36 +0600

    payment integration and handle error

--- src/pages/Dashboard/Payment/CheckoutForm.js
+++ src/pages/Dashboard/Payment/CheckoutForm.js
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert, Button, Container, Snackbar } from '@mui/material';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 
-const CheckoutForm = () => {
+
+const CheckoutForm = ({ order }) => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('')
+
+    // alert set up
+    const handleClose = (reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        // setOpen(false);
+        setSuccessMessage(false)
+        setErrorMessage(false)
+    };
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+        const card = elements.getElement(CardElement);
+        if (card == null) {
+            return;
+        }
+
+        // create payment method
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        })
+        if (error) {
+            setErrorMessage(error?.message)
+
+        } else {
+            if (paymentMethod.id) {
+                setSuccessMessage('Payment Complete Successfully!')
+            }
+        }
+    }
+
     return (
         <div>
-
+            <Container>
+                <form style={{ textAlign: 'center', color: 'crimson' }} onSubmit={handleSubmit}>
+                    <CardElement
+                        options={{
+                            style: {
+                                base: {
+                                    fontSize: '16px',
+                                    color: '#424770',
+                                    '::placeholder': {
+                                        color: '#aab7c4',
+                                    },
+                                },
+                                invalid: {
+                                    color: '#9e2146',
+                                },
+                            },
+                        }}
+                    />
+                    <Button type="submit" disabled={!stripe}>
+                        Pay ${order.price}
+                    </Button>
+                </form>
+            </Container>
+            {/* snackbar */}
+            <Snackbar open={successMessage} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    {successMessage}
+                </Alert>
+            </Snackbar>
+            {/* snackbar */}
+            <Snackbar open={errorMessage} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                    {errorMessage}
+                </Alert>
+            </Snackbar>
         </div>
     );
 };
--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51JxmMsFeiyYXbAR2bhYELH7vj4QB5OM0X5MV75kH4LZf4hNles2nGhujkvvzAAVU2iA5jkRQ8w0EEBRemSACOetN00VZJpS3Op')
 
 const Payment = () => {
     const { orderId } = useParams();
@@ -14,7 +19,11 @@ const Payment = () => {
         <div>
             <h2 className="fw-bold text-center pt-3 pb-2">Payment For: {order?.name}</h2>
             <h5 className="fw-bold text-center text-danger pt-3 pb-2">Please pay: ${order?.price}</h5>
-
+            <Elements stripe={stripePromise}>
+                <CheckoutForm
+                    order={order}
+                />
+            </Elements>
         </div>
     );
 };
